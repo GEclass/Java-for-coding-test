553a6caf799d6c00ea5ea89cc3ca0fcd27e08463
eunbileeme <pask2202@gmail.com>
2023-05-21 20:42:11 +0900

    거스름돈

---
+++ src/Greed_EX3_1.java
@@ -0,0 +1,17 @@
+public class Greed_EX3_1 {
+    public static void main(String[] args){
+        int n = 1260;
+        int[] coin_type = {500, 100, 50, 10};
+
+        int total_cnt = 0;
+        int coin_cnt = 0;
+
+        for (int k : coin_type) {
+            coin_cnt = n / k;
+            n -= (k * coin_cnt);
+            total_cnt += coin_cnt;
+        }
+
+        System.out.println(total_cnt);
+    }
+}
