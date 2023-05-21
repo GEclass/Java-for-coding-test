9cd7c8aaba36b4ca1d49f12c66d89c6ca96dde51
eunbileeme <pask2202@gmail.com>
2023-05-21 20:31:43 +0900

    1이 될 때까지

---
+++ src/Test4.java
@@ -0,0 +1,30 @@
+// 1. 자바에도 동일하게 while()문이 존재한다.
+// while () { }
+
+import java.util.*;
+
+public class Test4 {
+
+    public static void main(String [] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int result = 0;
+
+        while(n >= k) {
+            while (n % k != 0) {
+                n = n - 1;
+                result = result + 1;
+            }
+            n = n / k;
+            result = result + 1;
+        }
+        while (n > 1) {
+            n = n - 1;
+            result = result + 1;
+        }
+        System.out.println(result);
+    }
+}
