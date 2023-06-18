41695b2b740d9b474948b74aad92b0961739ec19
eunbileeme <pask2202@gmail.com>
2023-06-19 02:01:45 +0900

    정렬 라이브러리 키(Key) 기준 정렬

--- src/silver/sort/Test6.java
+++ src/silver/sort/Test6.java
@@ -19,14 +19,15 @@ class Fruit implements Comparable<Fruit> {
         return this.score;
     }
 
-    // Q. 디버깅 시, other.score이 왜 2, 5, 5, 2 순으로 할당되는지 모르겠습니다.
     @Override
     public int compareTo(Fruit other) {
-        System.out.println("this.score : " + this.score + ", other.score : " + other.score);
+        // System.out.println("this.score : " + this.score + ", other.score : " + other.score);
+        //
         if (this.score < other.score) {
             return -1;
         }
-        else if (this.score > other.score) {
+        //
+        else if (this.score > other.score) { // 5 > 2
             return 1;
         }
         // 두 객체의 score이 같을 경우
@@ -39,8 +40,8 @@ public class Test6 {
         // Fruit라는 클래스 타입을 가진 List 선언
         List<Fruit> fruits = new ArrayList<>();
 
-        fruits.add(new Fruit("바나나", 2));
-        fruits.add(new Fruit("사과", 5));
+        fruits.add(new Fruit("바나나", 2)); // other
+        fruits.add(new Fruit("사과", 5)); // this
         fruits.add(new Fruit("당근", 3));
 
         Collections.sort(fruits);
