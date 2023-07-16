888a128498b654fa4e9f1c41b9efad630eca549a
yelinkin <yelim785@gmail.com>
2023-07-16 23:40:09 +0900

    개선된 다익스트라 충돌 문제

--- src/yelim/path/SP_EX9_2.java
+++ src/yelim/path/SP_EX9_2.java
@@ -1,25 +1,18 @@
 package yelim.path;
-
 import java.util.*;
-
-class Node_queue implements Comparable<Node> {
-
+class Node implements Comparable<Node> {
     private int index;
     private int distance;
-
-    public Node_queue(int index, int distance) {
+    public Node(int index, int distance) {
         this.index = index;
         this.distance = distance;
     }
-
     public int getIndex() {
         return this.index;
     }
-
     public int getDistance() {
         return this.distance;
     }
-
     // 거리(비용)가 짧은 것이 높은 우선순위를 가지도록 설정
     @Override
     public int compareTo(Node other) {
@@ -29,7 +22,7 @@ class Node_queue implements Comparable<Node> {
         return 1;
     }
 }
-public class 요SP_EX9_2 {
+public class SP_EX9_2 {
     public static final int INF = (int) 1e9; // 무한을 의미하는 값으로 10억을 설정
     // 노드의 개수(N), 간선의 개수(M), 시작 노드 번호(Start)
     // 노드의 개수는 최대 100,000개라고 가정
@@ -38,15 +31,14 @@ public class 요SP_EX9_2 {
     public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
     // 최단 거리 테이블 만들기
     public static int[] d = new int[100001];
-
     public static void dijkstra(int start) {
-        PriorityQueue<Node_queue> pq = new PriorityQueue<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         // 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-        pq.offer(new Node_queue(start, 0));
+        pq.offer(new Node(start, 0));
         d[start] = 0;
         while(!pq.isEmpty()) { // 큐가 비어있지 않다면
             // 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-            Node_queue node = pq.poll();
+            Node node = pq.poll();
             int dist = node.getDistance(); // 현재 노드까지의 비용
             int now = node.getIndex(); // 현재 노드
             // 현재 노드가 이미 처리된 적이 있는 노드라면 무시
@@ -57,23 +49,20 @@ public class 요SP_EX9_2 {
                 // 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
                 if (cost < d[graph.get(now).get(i).getIndex()]) {
                     d[graph.get(now).get(i).getIndex()] = cost;
-                    pq.offer(new Node_queue(graph.get(now).get(i).getIndex(), cost));
+                    pq.offer(new Node(graph.get(now).get(i).getIndex(), cost));
                 }
             }
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
         m = sc.nextInt();
         start = sc.nextInt();
-
         // 그래프 초기화
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<Node>());
         }
-
         // 모든 간선 정보를 입력받기
         for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
@@ -82,13 +71,10 @@ public class 요SP_EX9_2 {
             // a번 노드에서 b번 노드로 가는 비용이 c라는 의미
             graph.get(a).add(new Node(b, c));
         }
-
         // 최단 거리 테이블을 모두 무한으로 초기화
         Arrays.fill(d, INF);
-
         // 다익스트라 알고리즘을 수행
         dijkstra(start);
-
         // 모든 노드로 가기 위한 최단 거리를 출력
         for (int i = 1; i <= n; i++) {
             // 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
